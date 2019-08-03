d361ea664bf835f748d0eeaeca1dc2cdc23dec79
brit-starks <brittneytechiethomas@gmail.com>
2019-08-02 19:30:03 -0700

    Add state

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,6 +5,13 @@ import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  
+  const [films, setFilms] = useState();
+  const [people, setPeople] = useState();
+  const [planets, setPlanets] = useState();
+  const [species, setSpecies] = useState();
+  const [starships, setStarships] = useState();
+  const [vehicles, setVehicles] = useState();
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
